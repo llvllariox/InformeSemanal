258414b40a3f7fbae4906062fda96df59fc511d4
[Mario Nuñez] <[mario.nunezf@gmail.com]>
2020-07-31 09:53:47 -0400

    capacity

--- src/app/components/ver-capacity/ver-capacity.component.ts
+++ src/app/components/ver-capacity/ver-capacity.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CapacityService } from '../../services/capacity.service';
+declare function init_customJS();
+
 
 @Component({
   selector: 'app-ver-capacity',
@@ -9,10 +11,15 @@ import { CapacityService } from '../../services/capacity.service';
 export class VerCapacityComponent implements OnInit {
 
   constructor(public capacityService: CapacityService ) {
-    
+    init_customJS();
   }
 
   ngOnInit(): void {
+  }
+
+  eliminar(i){
+    this.capacityService.jsonDataPlanService.splice(i, 1);
+    this.capacityService.totalesDia();
   }
 
 }
--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -12,3 +12,10 @@ function init_customJS() {
         });
     });
 }
+
+$(window).on("load resize ", function() {
+    var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
+    $('.tbl-header').css({
+        'padding-right': scrollWidth
+    });
+}).resize();
